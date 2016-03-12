5e3833ee3e564b8ef5b1ed3bbe824b085de294bc
bill-jia <bill.jiazy@gmail.com>
2016-03-12 16:58:14 +0000

    Add signup to app based on spotify user info User info added by API query and saved to app's database

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager, UserMixin
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///spotty.db'
 db = SQLAlchemy(app)
 lm = LoginManager(app)
 
 
-from app import auth
+from app import auth, models
 
--- app/auth.py
+++ app/auth.py
@@ -4,11 +4,11 @@ import spotipy.util as util
 
 from flask import redirect, url_for
 from flask.ext.login import LoginManager, current_user
-from app import app, lm
+from app import app, lm, db
 
 from .models import User
 
-scope = 'user-library-read user-follow-read'
+scope = 'user-library-read user-follow-read user-read-email'
 
 @lm.user_loader
 def load_user(_id):
@@ -21,14 +21,27 @@ def index():
 
 @app.route('/authorize/<username>')
 def authorize(username):
+	"""
+	Requests OAuth from Spotify and signs up new users onto database. Updates users' lists of
+	songs, playlists, artists.
+	"""
 	if current_user.is_anonymous:
 		token = util.prompt_for_user_token(username, scope)
 		if token:
 			sp = spotipy.Spotify(auth=token)
-			artists = sp.current_user_followed_artists()
-			tracks = sp.current_user_saved_tracks()
-			playlists = sp.user_playlists(username)
-			print(artists)
+			user = sp.current_user()
+			print(user)
+			# If the user does not exist on our database, register them
+			if User.query.filter_by(spotify_id=user['id']).first() == None:
+				newUser = User(name=user['display_name'], spotify_id=user['id'], email=user['email'])
+				db.session.add(newUser)
+				db.session.commit()
+				print("new user registered!")
+			else:
+				print("user already exists")
+			#update data in our database
+			#updateSpotifyData(sp)
+
 		else:
 			print("authentication failed!")
 	return redirect(url_for('index'))
