714e892fe4b94af99b17d39201ecf087e09d0c3b
bill-jia <bill.jiazy@gmail.com>
2016-03-12 18:14:14 +0000

    Add Spotify API parsing for user data Note that data is not yet saved to database.

--- app/auth.py
+++ app/auth.py
@@ -40,8 +40,58 @@ def authorize(username):
 			else:
 				print("user already exists")
 			#update data in our database
-			#updateSpotifyData(sp)
+			updateSpotifyData(sp)
 
 		else:
 			print("authentication failed!")
 	return redirect(url_for('index'))
+
+def updateSpotifyData(spotify):
+	"""
+	Update songs, playlists, artists upon login
+	"""
+	lim = 50
+	artists = spotify.current_user_followed_artists(limit=lim)
+	totalArtists = artists["artists"]["total"]
+	if totalArtists > lim:
+		pages = int((totalArtists-(lim+1))/lim)+1
+		for i in range(1,pages):
+			nextCursor = artists["artists"]["cursors"]["after"]
+			artists = spotify.current_user_followed_artists(limit=lim, after=nextCursor)
+	
+	tracks = spotify.current_user_saved_tracks(limit=lim)
+	totalTracks = tracks["total"]
+	# for track in tracks["items"]:
+	# 	print(track["track"]["name"])
+	if totalTracks > lim:
+		pages = int((totalTracks-(lim+1))/lim)+1
+		for i in range(1,pages):
+			tracks = spotify.current_user_saved_tracks(limit=lim, offset=i*lim)
+			# for track in tracks["items"]:
+			# 	print(track["track"]["name"].encode('utf-8'))			
+
+	userId = spotify.current_user()["id"]
+	playlists = spotify.user_playlists(userId, limit=lim)
+	totalPlaylists = playlists["total"]
+	getTracksFromPlaylists(playlists, spotify)
+	if totalPlaylists > lim:
+		pages = int((totalPlaylists-(lim+1))/lim)+1
+		for i in range(1,pages):
+			playlists = spotify.user_playlists(userId, limit=lim, offset=i*lim)
+			getTracksFromPlaylists(playlists, spotify)
+
+def getTracksFromPlaylists(playlists, spotify):
+	"""
+	Get all the songs off a group fo playlists
+	"""
+	lim = 100
+	for playlist in playlists["items"]:
+		if playlist["public"]:
+			playlistId = playlist["id"]
+			userId = playlist["owner"]["id"]
+			tracks = spotify.user_playlist_tracks(userId, playlistId, limit=lim)
+			totalTracks = tracks["total"]
+			if totalTracks > lim:
+				pages = int((totalTracks-(lim+1))/lim)+1
+				for i in range(1,pages):
+					tracks = spotify.user_playlist_tracks(userId, playlistId, limit=lim, offset=i*lim)
